0aa762e5772c46e5df480654bfc947a6ac4737b3
Muhammad Ismail <ismardoi2001@gmail.com>
2020-08-06 12:53:19 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="mainheader"> 
+      <div className="logo">
+      </div>
+      <nav>
+      <a href="@">Home</a>
+      <a href="@">Services</a>
+      <a href="@">About</a>
+      <a href="@">About</a>
+      </nav>
+      <div className="menubtn">
+      <button> HelpLine</button>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
