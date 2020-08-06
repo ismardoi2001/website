b1fa0c85a66d19e747a760b1fadb546a2bb8c9d5
Muhammad Ismail <ismardoi2001@gmail.com>
2020-08-06 19:47:45 +0500

    update task

--- src/App.js
+++ src/App.js
@@ -12,16 +12,13 @@ function App() {
       <a href="@">Home</a>
       <a href="@">Services</a>
       <a href="@">About</a>
-      <a href="@">About</a>
+      <a href="@">Contact</a>
       </nav>
-      <div className="menubtn">
-      <button> HelpLine</button>
-      </div>
+      
       </div>
       <main className="main">
       <section className="left-sec">
-      <h2>We Are Here For Your help</h2>
-      <h1>We AreBest Developers</h1>
+      <h1><span>W</span>e Are Best<span> De</span>velopers</h1>
       <p> We are here for your help 24/7 just any help call us</p>
       <button> Make An Appointment</button>      
       </section>
