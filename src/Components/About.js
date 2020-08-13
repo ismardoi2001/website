981d9b68eff8747f710a0bccb7968b5da134135b
Muhammad Ismail <ismardoi2001@gmail.com>
2020-08-13 15:15:24 +0500

    update tsak

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,6 +15,7 @@ export const About = () => {
       <div class="col-right">
         <h1 class="section-title">About <span>me</span></h1>
         <h2>Front End Developer</h2>
+        
         <p> I am  Mobile & Web Front End Developer My Expertise in Mobile & Website Development and Design HTML 5 CSS 3 JavasScript  JQuery Bootstrap 4 React.Native Web Animation API, TypeScript and Embared Technologies with help of API Technology Provide Services in Mobile Apps Development Graphic Design Software Development Website Design Digital Markting SEO Front End Developer!</p>
         <a href="#" class="cta">Download Resume</a>
       </div>
